0ac1c650235a841b505fcc0ae054ef1a91f36159
Khan Shuvo <s1900163@edu.bc.fi>
2019-08-10 19:47:31 +0300

    untracked files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class App extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state={
 
-export default App;
+    }
+  }
+  render() { //Declaring  Array Elements
+    const elements = [
+      {name:'Mari',
+        age:29
+      },
+      {name:'Anne',
+        age:25
+      },
+      {name:'Kati',
+        age:45
+      },
+    ];
+
+    const items = [] //Rerndering html
+
+    for (const [index, value] of elements.entries()) {
+      items.push( <div class="flex" key={index}>
+          <p class="name">{value.name}</p>
+          <p class="details">Age:{value.age}</p>
+          </div>)
+    }
+    return (
+        <div id="flex">
+        {items}
+        </div>
+  )
+  }
+}
